006bfa1f43e551cd260cb4b8fe955591b653bad4
JP-sDEV <jon.pablo80@gmail.com>
2025-05-21 15:08:15 -0400

    config change to use db url

--- config/database.js
+++ config/database.js
@@ -4,18 +4,21 @@ module.exports = ({ env }) => {
   const connections = {
     postgres: {
       connection: {
+        // Prefer DATABASE_URL if it's defined
         connectionString: env("DATABASE_URL"),
+
+        // Only use the following if connectionString is NOT defined
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
         ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          key: env("DATABASE_SSL_KEY"),
+          cert: env("DATABASE_SSL_CERT"),
+          ca: env("DATABASE_SSL_CA"),
+          capath: env("DATABASE_SSL_CAPATH"),
+          cipher: env("DATABASE_SSL_CIPHER"),
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
             true
