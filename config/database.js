22944353d81f13bfe5d88676de7dc99602003314
JP-sDEV <jon.pablo80@gmail.com>
2025-05-21 15:16:31 -0400

    config change

--- config/database.js
+++ config/database.js
@@ -1,42 +1,40 @@
 module.exports = ({ env }) => {
   const client = env("DATABASE_CLIENT", "sqlite");
 
-  const connections = {
-    postgres: {
-      connection: {
-        // Prefer DATABASE_URL if it's defined
-        connectionString: env("DATABASE_URL"),
+  const useConnectionString = !!env("DATABASE_URL");
 
-        // Only use the following if connectionString is NOT defined
+  const connection = useConnectionString
+    ? {
+        connectionString: env("DATABASE_URL"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+      }
+    : {
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
         ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY"),
-          cert: env("DATABASE_SSL_CERT"),
-          ca: env("DATABASE_SSL_CA"),
-          capath: env("DATABASE_SSL_CAPATH"),
-          cipher: env("DATABASE_SSL_CIPHER"),
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
             true
           ),
         },
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-  };
+      };
 
   return {
     connection: {
       client,
-      ...connections[client],
+      connection,
+      pool: {
+        min: 0,
+        max: 5, // Supabase has a low connection limit
+      },
       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
   };
